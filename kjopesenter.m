0e95480d903fccbe907fd08a4ecf40efb9c1a3c6
Benedictenils <112081507+Benedictenils@users.noreply.github.com>
2022-10-04 11:14:11 +0200

    Occupancy grid
    Laget i forbindelse med arbeid med lattice. En matrise hvor ledig plass er 0 og utilgjengelig plass er 1

---
+++ kjopesenter.m
@@ -0,0 +1,11 @@
+
+%%Lager kjopesenter 
+
+%Lager tom matrise for å illustrere kjøpesenteret 
+kjopesenter=zeros(80,160);
+
+%Ligger inn okkuperte områder 
+kjopesenter(50:80,[1:50])=1;     %(fra_rad:til_rad,[fra_kolonne:til_kolonne])
+
+
+                            
